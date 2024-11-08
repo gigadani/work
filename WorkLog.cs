d048c86d3d5d2bf05057637b0047026e236cba24
Daniel Ahrari <daniel.ahrari@protonmail.com>
2024-11-09 00:12:25 +0200

    Add project files.

---
+++ WorkLog.cs
@@ -0,0 +1,37 @@
+﻿using hssl.Models;
+using System.Text.Json;
+
+namespace hssl;
+
+public class WorkLog
+{
+    public string FilePath { get; set; }
+
+    public WorkLog(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    public void LogAction(WorkLogAction action)
+    {
+        LogInFile = LogInFile.Append(action);
+    }
+
+    private IEnumerable<WorkLogAction> LogInFile
+    {
+        get
+        {
+            if (!File.Exists(FilePath))
+            {
+                throw new FileNotFoundException("File not found", FilePath);
+            }
+            return JsonSerializer.Deserialize<WorkLogAction[]>(File.ReadAllText(FilePath)) ?? [];
+        }
+        set
+        {
+            File.WriteAllText(FilePath, JsonSerializer.Serialize(value));
+        }
+    }
+
+    public WorkLogAction? LatestAction => LogInFile.LastOrDefault();
+}
